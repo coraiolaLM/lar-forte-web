7f96dce043b0b2f7b63ca7ff84bc62ba69aff582
Bruno Coraiola <bru.coraiola@gmail.com>
2026-09-01 21:30:29 -0300

    Corrige conflito de versões do Puppeteer

--- server.js
+++ server.js
@@ -88,13 +88,24 @@ const limitador = rateLimit({
 // ============================================
 
 console.log('🚀 Iniciando cliente do WhatsApp...');
+const chromePath = process.env.PUPPETEER_EXECUTABLE_PATH;
+
+if (!chromePath) {
+    console.error('❌ PUPPETEER_EXECUTABLE_PATH não foi configurado.');
+}
+
+console.log('🌐 Chrome configurado em:', chromePath);
 
 const client = new Client({
     authStrategy: new LocalAuth({
         clientId: 'lar-forte'
     }),
+
     puppeteer: {
         headless: true,
+
+        executablePath: chromePath,
+
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
